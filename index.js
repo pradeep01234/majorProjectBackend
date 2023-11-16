92960f6c7ca4534ab414ec130d089d63869d6b7a
pradeep01234 <pradeep9340@outlook.com>
2023-11-16 13:39:43 +0530

    core issue resolved

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const routes = require("./Route/routes");
+const cors = require("cors");
 
+app.use(cors())
 app.use(express.json())
 app.use("/",routes);
 app.listen(process.env.PORT,()=>{
